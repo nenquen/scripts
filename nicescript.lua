42ea2ecd37986e964a22badacffa14d4990658e2
nenquen <96012695+nenquen@users.noreply.github.com>
2025-04-29 05:25:40 +0300

    Create nicescript.lua

---
+++ nicescript.lua
@@ -0,0 +1,17 @@
+local ScreenGui = Instance.new("ScreenGui")
+local TextLabel = Instance.new("TextLabel")
+
+ScreenGui.Parent = game.CoreGui
+TextLabel.Parent = ScreenGui
+TextLabel.Size = UDim2.new(1, 0, 1, 0)
+TextLabel.Position = UDim2.new(0, 0, 0, 0)
+TextLabel.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BackgroundTransparency = 0.5
+TextLabel.Text = "Go touch grass nigger!"
+TextLabel.TextColor3 = Color3.fromRGB(255, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.Font = Enum.Font.SourceSansBold
+
+wait(5)
+
+game.Players.LocalPlayer:Kick("Stop using scripts idiot!!!")
